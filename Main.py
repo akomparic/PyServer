c79e000e89e318c4b2f5973c0dce3a0b241ed523
Aleksandar Komparic <lordkilllalot@gmail.com>
2014-09-04 11:32:06 +0200

    App with auth test2

--- Main.py
+++ Main.py
@@ -2,49 +2,42 @@ __author__ = 'aleksandar'
 
 
 import re
-from cgi import escape
 
 
-def index(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return ['Missing arguments']
+class Selector(object):
 
-
-def authentication(environ, start_response):
-        args = environ['test.url_args']
-        if args:
-            subject = escape(args[0])
-        else:
-            subject = 'Void'
+    def index(self, environ, start_response):
         start_response('200 OK', [('Content-Type', 'text/html')])
-        return ['''Auth wait %(subject)s ''' % {'subject': subject}]
+        return ['Missing arguments']
 
+    def authentication(self, environ, start_response):
+        start_response('200 OK', [('Content-Type', 'text/html')])
+        return ['Authentication temp']
 
-def not_found(environ, start_response):
-    start_response('404 NOT FOUND', [('Content-Type', 'text/plain')])
-    return ['Not found']
+    def not_found(self, environ, start_response):
+        start_response('404 NOT FOUND', [('Content-Type', 'text/plain')])
+        return ['Not found']
 
-urls = [
-    (r'^$', index),
-    (r'auth/?', authentication),
-    (r'auth/(.+)$', authentication)
-]
+    urls = [
+        (r'^$', index),
+        (r'auth/?', authentication),
+        (r'auth/(.+)$', authentication)
+    ]
 
-
-def main_app(environ, start_response):
-    path = environ.get('PATH_INFO', '').lstrip('/')
-    for regex, callback in urls:
-        match = re.search(regex, path)
-        if match is not None:
-            environ['test.url_args'] = match.groups()
-            return callback(environ, start_response)
-    return not_found(environ, start_response)
+    def main_app(self, environ, start_response):
+        path = environ.get('PATH_INFO', '').lstrip('/')
+        for regex, callback in self.urls:
+            match = re.search(regex, path)
+            if match is not None:
+                environ['test.url_args'] = match.groups()
+                return callback(self, environ, start_response)
+        return self.not_found(environ, start_response)
 
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
-    httpd = make_server('localhost', 8080, main_app)
-    print('Serving on port 8080...')
+    httpd = make_server('localhost', 8090, Selector().main_app)
+    print('Serving on port 8090...')
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
